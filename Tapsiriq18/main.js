fda35bb42eeb8b3a9b6b6c4f22a6b3eca263fdc8
Ramles75 <elmarseyidov76@gmail.com>
2024-01-12 17:20:16 +0400

    file add

---
+++ Tapsiriq18/main.js
@@ -0,0 +1,98 @@
+// Task-1
+
+// let str = "My name is Elmar";
+
+// console.log(str);
+
+
+
+// Task-2
+
+// const name = "Elmar";
+
+// console.log(name);
+
+
+
+// Task-3
+
+// let str = "person, $add";
+
+// console.log(str);
+
+
+
+// Task-4
+
+// let bookPrice = 750;
+// let bookAmount = 14;
+// let totalPrice = bookPrice * bookAmount;
+
+// console.log(totalPrice);
+
+
+
+// Task-5
+
+// let kmhSpeed = 75;
+// let msSpeed = kmhSpeed * 10 / 36;
+
+// console.log(msSpeed);
+
+
+
+// Task-6
+
+// let year = prompt("Doğum ilini daxil edin");
+// const date = new Date()
+// let nowYear = date.getFullYear();
+// let age = nowYear - year;
+
+// console.log("Bu günə qədər yaşadığınız günlərin sayı: " + age * 365);
+
+
+
+// Task-7
+
+// let a = 3
+// let b = 5
+
+// console.log(a ** b);
+
+// console.log(b % a);
+
+
+
+// Task-8
+
+// let firstName = "Alan";
+// let lastName = "Turing";
+
+// console.log(`Sizin adınız ${firstName} ${lastName}. Tanışdığımıza çox şadam!`);
+
+
+
+// Task-9
+
+// let firstName = "Elmar";
+// let lastName = "Seyidov";
+
+// console.log(`Sizi bir daha görməyimizə şadıq, ${firstName} ${lastName}.`);
+
+
+
+// Task-16
+
+// "" + 1 + 0 = 10
+// "" - 1 + 0 = -1
+// "true" - false = NaN
+// 6 / "3" = 2
+// 4 + 5 + "px" = 9px
+// "$" + 4 + 5 = $45
+// "4" - 2 = 2
+// "4px" - 2 + "px" = NaNpx
+// 7 / 0 = Infinity
+// "-9" + 5 = -95
+// "-9" - 5 = -14
+// null + 1 = 1
+// undefined + 1 = NaN
