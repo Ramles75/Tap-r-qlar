722b0acdd6d1431cb20b51bd9b773d5d13943e89
Ramles75 <elmarseyidov76@gmail.com>
2024-01-26 14:33:37 +0400

    code add

--- Tapsiriq18/main.js
+++ Tapsiriq18/main.js
@@ -96,3 +96,67 @@
 // "-9" - 5 = -14
 // null + 1 = 1
 // undefined + 1 = NaN
+
+
+
+// Task-30
+
+// let value = "2009.11.19";
+// let valueArr = value.split(".");
+// let year = valueArr[0];
+// let month = valueArr[1];
+// let day = valueArr[2];
+
+// switch (month) {
+//     case "1":
+//     case "01":
+//         month = "Yanvar"
+//         break;
+//     case "2":
+//     case "02":
+//         month = "Fevral"
+//         break;
+//     case "3":
+//     case "03":
+//         month = "Mart"
+//         break;
+//     case "4":
+//     case "04":
+//         month = "Aprel"
+//         break;
+//     case "5":
+//     case "05":
+//         month = "May"
+//         break;
+//     case "6":
+//     case "06":
+//         month = "Iyun"
+//         break;
+//     case "7":
+//     case "07":
+//         month = "Iyul"
+//         break;
+//     case "8":
+//     case "08":
+//         month = "Avqust"
+//         break;
+//     case "9":
+//     case "09":
+//         month = "Sentyabr"
+//         break;
+//     case "10":
+//         month = "Oktyabr"
+//         break;
+//     case "11":
+//         month = "Noyabr"
+//         break;
+//     case "12":
+//         month = "Dekabr"
+//         break;
+//     default:
+//         break;    
+// }
+
+
+// console.log(`${day} ${month} ${year}`);
+
