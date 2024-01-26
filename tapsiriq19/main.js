abb6830210c30706090d72fcb26800e9674af96f
Ramles75 <elmarseyidov76@gmail.com>
2024-01-26 18:50:31 +0400

    file add

---
+++ tapsiriq19/main.js
@@ -0,0 +1,57 @@
+// Task-1
+
+// let i = 73;
+// while (i < 100) {
+
+//     i = i + 7
+// }
+
+// console.log(i);
+
+// sadecə sonuncu yerinə, bütün iki rəqəmli ədələri göstərir.
+
+
+
+
+// Task-2
+
+// let i = +prompt()
+// while (i > 0) {
+//     console.log("I know how to use cycles", i);
+//     i--
+// }
+
+
+
+
+// Task-3
+
+// let i = 990;
+// while (i >= 100) {
+//     console.log(i);
+//     i = i - 10
+// }
+
+
+
+
+// Task-4
+
+// let i = 99
+// let cem = 0;
+// while (i > 0) {
+//     if (i % 2 == 1) {
+//         cəm += i
+//     }
+//     console.log(i);
+//     i--
+// }
+
+let cem = 0;
+for (let i = 10; i > 0; i--) {
+    if (i % 2 == 1) {
+        cem += i;
+    }
+}
+
+console.log(cem);
