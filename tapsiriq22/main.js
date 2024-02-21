4d364216e01fa6cc781c73ca41b22cfed72b0493
Ramles75 <elmarseyidov76@gmail.com>
2024-02-21 23:15:03 +0400

    file add

---
+++ tapsiriq22/main.js
@@ -0,0 +1,14 @@
+// Task-01
+
+// const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// const [firstNumber, secondNumber, ...restNumbers] = numbers;
+
+// let newArray = restNumbers;
+
+// console.log("First number:", firstNumber);
+// console.log("Second number:", secondNumber);
+// console.log("Other numbers:", newArray);
+
+
+
