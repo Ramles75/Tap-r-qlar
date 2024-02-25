8809c90c32d51d01e30939ab4974724da6eb7f83
Ramles75 <elmarseyidov76@gmail.com>
2024-02-25 23:52:56 +0400

    file add

---
+++ tapsiriq23/main.js
@@ -0,0 +1,40 @@
+// Task-01
+
+// let user = {
+//     name: "Elmar",
+//     gender: "Erkek",
+//     birthday: "05.07.2004"
+// }
+
+// console.log(user.birthday);
+
+// Task-02
+
+// let user = {
+//     name: "Elmar",
+//     gender: "Erkek",
+//     birthday: "05.07.2004"
+// }
+
+// user.age = 19;
+// delete user.birthday;
+// console.log(user);
+
+// Task-03
+
+// let user = {
+//     name: "Elmar",
+//     gender: "Erkek",
+//     birthday: "05.07.2004",
+//     age: 19,
+
+//     setAge: function (newAge) {
+//         user.age = newAge
+//     },
+//     getyearbeforeretirement: function (params) {
+//         return 65 - user.age
+//     }
+// }
+
+// user.setAge(19);
+// console.log(user.getyearbeforeretirement());
