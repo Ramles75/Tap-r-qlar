bc85cb38eb917d6139ef785c1f84fc79b2984af6
Ramles75 <elmarseyidov76@gmail.com>
2024-03-01 18:27:24 +0400

    file add

---
+++ tapsiriq25/main.js
@@ -0,0 +1,17 @@
+// Task-01
+
+// document.querySelector("li");
+// console.log(document.querySelector("li"));
+
+
+
+// Task-02
+
+// const element = document.querySelector("h1");
+
+// element.remove();
+
+// const elem = document.querySelector("ul");
+// console.log(elem.innerHTML);
+
+// elem.innerHTML += "<li>TITAN RTX</li>"
