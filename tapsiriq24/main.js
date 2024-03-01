3376e41fec80d7b248b07a9b0d9d3988abe75666
Ramles75 <elmarseyidov76@gmail.com>
2024-03-01 07:40:18 +0400

    file add

---
+++ tapsiriq24/main.js
@@ -0,0 +1,16 @@
+// Task-01
+
+// let friends = ["Cavid", "Elgun", "Melik"];
+// friends.push("Leyla");
+// friends.shift();
+
+// console.log(friends);
+
+
+
+// Task-02
+
+// let Numbers = [30, 16, 23, 87, 67, 45, 10, 29, 34, 71];
+// let maxValue = Math.max(...Numbers);
+
+// console.log(maxValue);
